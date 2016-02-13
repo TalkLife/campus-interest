24ed176721de8f7dbfe97d5caad1a6f18aebe1e5
Jamie Davies <viralpickaxe@gmail.com>
2016-02-13 12:14:16 +0000

    Basic app structure

---
+++ build/bundle.js
@@ -0,0 +1,20 @@
+webpackJsonp([1],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(81);
+
+
+/***/ },
+
+/***/ 81:
+/***/ function(module, exports) {
+
+	'use strict';
+
+	console.log('hi');
+
+/***/ }
+
+});
